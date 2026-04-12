8ca06a37b028a68139f1c1a5a8f601299df402ce
Henry Bernardin <Henry.Bernardin@gmail.com>
2026-04-12 19:26:59 -0400

    feat: build calculator layout and implement operation helpers

--- script.js
+++ script.js
@@ -16,4 +16,29 @@ const divide = function(x, y){
     return x / y; 
 }
 
+let number1 = 0; 
+let number2 = 0; 
+const operator = {
+    addition:  "+",
+    subtraction: "-", 
+    multiplication: "*", 
+    division: "/", 
+}
 
+const operate = function(number1, operator, number2){
+if(operator === operator.addition){
+return add(number1,number2);
+}
+if(operator === operator.subtraction){
+    return subtract(number1, number2); 
+}
+if (operator === operator.multiplication) {
+    return multiply(number1, number2); 
+}
+if (operator === operator.division) {
+    return divide(number1, number2); 
+}
+}
+
+xc
+const click = addEventListener(onclick, )
